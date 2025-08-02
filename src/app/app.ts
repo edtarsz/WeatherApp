81c543d8cb080abf86f5e734339cf120fd7f1a01
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-08-01 17:02:37 -0700

    Prueba final posiblemente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./shared/header/header";
-import { Footer } from "./shared/footer/footer";
-import { Main } from "../main/main";
-import { LandingPage } from "../landing.page/landing.page";
-import { Account } from "../account/account";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Main, LandingPage, Account],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
