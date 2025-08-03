5878d71102e03f408ad9cad6eaecc28ce8671e18
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-08-02 21:41:18 -0700

    Menu del header responsive

--- src/app/core/services/interface.service.ts
+++ src/app/core/services/interface.service.ts
@@ -7,9 +7,16 @@ export class InterfaceService {
     private _showSearchBar = signal(false);
     public showSearchBar = this._showSearchBar.asReadonly();
 
+    private _showMenu = signal(false);
+    public showMenu = this._showMenu.asReadonly();
+
     constructor() { }
 
     public toggleSearchBar(): void {
         this._showSearchBar.update((value: boolean) => !value);
+    }
+
+    public toggleMenu(): void {
+        this._showMenu.update((value: boolean) => !value);
     }
 }
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -11,8 +11,13 @@ import { InterfaceService } from '../../core/services/interface.service';
 export class Header {
   private interfaceService = inject(InterfaceService);
   public showSearchBar = this.interfaceService.showSearchBar;
+  public showMenu = this.interfaceService.showMenu;
 
   public onSearchBarToggle(): void {
     this.interfaceService.toggleSearchBar();
+  }
+
+  public onMenuToggle(): void {
+    this.interfaceService.toggleMenu();
   }
 }
